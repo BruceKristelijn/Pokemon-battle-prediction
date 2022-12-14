0ecbf08bd5446763f294810bbb46da88f9e2cd90
Bram van Oosterhout <bls.place@gmail.com>
2022-12-14 14:08:58 +0100

    Updated with working script

--- random_forest_classifier.py
+++ random_forest_classifier.py
@@ -1,56 +1,47 @@
-# # Import the necessary libraries
-# from sklearn.ensemble import RandomForestClassifier
+# Import the necessary libraries
+from sklearn.model_selection import train_test_split
+#Import Random Forest Model
+from sklearn.ensemble import RandomForestClassifier
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
 
-# # Define the input data
-# # In this example, we have two Pokemon with the following stats:
-# # - Pokemon 1: level 10, attack 10, defense 10, speed 10
-# # - Pokemon 2: level 15, attack 15, defense 15, speed 15
-# x = [[10, 10, 10, 10],
-#      [15, 15, 15, 15]]
-
-# # Define the output data
-# # In this example, we have simulated the battle between the two Pokemon and
-# # determined that Pokemon 1 wins
-# y = [1, 0]
-
-# # Create the random forest model
-# model = RandomForestClassifier()
-
-# # Train the model on the input and output data
-# model.fit(x, y)
-
-# # Use the trained model to make predictions about the outcome of future battles
-# # For example, if we have a new Pokemon with the following stats:
-# # - Pokemon 3: level 12, attack 12, defense 12, speed 12
-# new_pokemon = [[6, 6, 6, 6]]
-# prediction = model.predict(new_pokemon)
-
-# # The prediction will be either 1 (if Pokemon 3 wins) or 0 (if Pokemon 3 loses)
-# print(prediction)
-
+import random
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 
-# load the data into a pandas dataframe
-df = pd.read_csv('pokemon_battles.csv')
+# Read pokemon
+data = pd.read_csv("pokemon_simulation_10k.csv")
 
-# create the features and target variables
-X = df[['pokemon1', 'pokemon2', 'level1', 'level2', 'hp1', 'hp2', 'attack1', 'attack2', 'defense1', 'defense2', 'speed1', 'speed2']]
-y = df['outcome']
+# Pre create values
+x = []
+y = []
 
-# create and fit the decision tree model
-model = DecisionTreeClassifier()
-model.fit(X, y)
+for index in data.index:
+     series = data.iloc[index]
+     winner = [series['winner_level'], series['winner_hp'], series['winner_attack'], series['winner_defense'], series['winner_speed']]
+     loser = [series['loser_level'], series['loser_hp'], series['loser_attack'], series['loser_defense'], series['loser_speed']]
 
-# make a prediction for a battle
-p1 = ['Charmander', 5, 100, 50, 50, 100]
-p2 = ['Squirtle', 5, 100, 50, 50, 50]
+     # print([winner, loser])
 
-prediction = model.predict([p1, p2])
+     # Randomize
+     if(random.randint(0, 2) == 1):
+          x.append(winner + loser)
+          y.append([1,0])
+     else:
+          x.append(loser + winner)
+          y.append([0,1])
 
-# print the result of the battle
-if prediction[0] == prediction[1]:
-    print("The battle is a tie!")
-else:
-    winner = prediction[0] if model.predict_proba([p1, p2])[0][0] > 0.5 else prediction[1]
-    print(f"{winner} wins the battle!")
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+print(clf.predict([[97, 9000, 0, 0, 0, 0, 98, 0, 0, 0]]))
